a44567b038e3b9d9c0d4dbc8a19fc290a63cef9e
Kiatsyndesi <kiyanitsadenis@gmail.com>
2024-09-08 21:26:10 +0300

    feat!: add all needed dependencies

---
+++ app/src/test/kotlin/AppTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+class AppTest {
+    @Test
+    fun appHasAGreeting() {
+        val classUnderTest = App()
+        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    }
+}
